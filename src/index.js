bebc4f895318ea9c9660ccca2893e7d642238737
Yfd_omibeaver <108644167+winterOmii@users.noreply.github.com>
2024-07-16 22:34:10 +0800

    up

--- src/index.js
+++ src/index.js
@@ -7,17 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.hantrends.life.com": dockerHub,
+  "quay.hantrends.life": "https://quay.io",
+  "gcr.hantrends.life": "https://gcr.io",
+  "k8s-gcr.hantrends.life": "https://k8s.gcr.io",
+  "k8s.hantrends.life": "https://registry.k8s.io",
+  "ghcr.hantrends.life": "https://ghcr.io",
+  "cloudsmith.hantrends.life.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
